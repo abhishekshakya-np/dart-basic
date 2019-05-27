fc9c8389647172fe797c6b88e99b01c7291ed517
Abishek <abishek12344321@gmail.com>
2019-05-27 12:01:17 +0545

    string,boolian, int, double ,list

---
+++ newcode.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int x = 10;
+  double y = 10.3;
+
+
+  String s = "${x-y}";
+  print(s);
+  bool b = true;
+   print(b);
+   List l = [1, 2, 3];
+   print(l[0]);
+   List<String> ls = ["1", "2", "3"];
+   print(ls[1]);
+}
